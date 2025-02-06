ef97636ca9045d21a23590d896bdeb20ff6718d8
Andrew Tamburino <57277787+atamburino@users.noreply.github.com>
2025-02-05 23:33:54 -0500

    Leetcode 75 one second way to solve

--- MergeStringsAlternately.js
+++ MergeStringsAlternately.js
@@ -13,3 +13,18 @@ var mergeAlternately = function(word1, word2) {
     }
     return merged;
 };
+
+
+///////////////////
+
+var mergeAlternately = function(word1, word2) {
+    const result = [];
+    const maxLength = Math.max(word1.length, word2.length);
+    
+    for (let i = 0; i < maxLength; i++) {
+        if (i < word1.length) result.push(word1[i]);
+        if (i < word2.length) result.push(word2[i]);
+    }
+    
+    return result.join('');
+};
