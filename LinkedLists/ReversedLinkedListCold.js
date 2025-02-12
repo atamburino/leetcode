61a93485e662d0ef857027e314c195f44cbc1486
Andrew Tamburino <57277787+atamburino@users.noreply.github.com>
2025-02-11 22:14:35 -0500

    cold reverse linked list cold

---
+++ LinkedLists/ReversedLinkedListCold.js
@@ -0,0 +1,13 @@
+var reverseList = function(head) {
+    let current = head;
+    let prev = null;
+
+    while (current != null) {
+        let nextNode = current.next;
+        current.next = prev;
+
+        prev = current
+        current = nextNode;
+    }
+    return prev;
+};
